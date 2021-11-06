338f58fe4ee757e0186a5c8255c32ca62f51731a
lkangr <dangquocthang721@gmail.com>
2021-11-06 15:14:59 +0700

    add Cart

--- restaurant-pos/src/MainPage/CartElement.js
+++ restaurant-pos/src/MainPage/CartElement.js
@@ -5,6 +5,7 @@ import CartInfo from './CartInfo';
 const CartElement = ({ hide }) => {
   return (
     <div className="container cart overflow-auto">
+      <div className="sticky-top" style={{backgroundColor: 'rgb(208, 236, 233)'}}>
       <div className="row">
         <div className="col-4 mt-2">
           <img src={cart} alt='cart icon' width='25px' height='25px' />
@@ -13,7 +14,8 @@ const CartElement = ({ hide }) => {
           <div className="col-4"></div>
           <button className="col-4 btn btn-secondary" onClick={hide}>Close</button>
         </div>
-      <p>_____________________________________________________</p>
+        <p>_____________________________________________________</p>
+      </div>
       <CartInfo />
     </div>
   );
