f78b6531e8bb7395f9ef11f82042c73850b29a51
thanhvodev <thanhvo.dev@gmail.com>
2021-11-12 22:21:30 +0700

    add login logout func

--- restaurant-pos/src/MainPage/MainPage.js
+++ restaurant-pos/src/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import Navtest from "./Nav-test";
 import home from "../img/home.png";
 import cart from "../img/cart.svg";
@@ -7,9 +7,12 @@ import useCart from "./useCart";
 import Cart from "../MainPage/Cart";
 import Register from "../MainPage/register";
 import { Link } from "react-router-dom";
+import { UserContext } from "../components/UserContext";
 
 function MainPage() {
     const { isShowing, toggle } = useCart(false);
+    const { user, setUser } = useContext(UserContext);
+
     return (
         <div className="menu">
             <div className="container-fluid  MainPage">
@@ -20,13 +23,17 @@ function MainPage() {
                                 <div id="Backtohome" className="col-9">
                                     <img id="home" src={home} alt="Home Icon" width="50px" height="50px" />
                                     <p>Back to Home</p>
+                                    <p className="bg-primary">Hello {user}</p>
                                 </div>
                                 <Link to="/register" className="btn btn-3 col-1">
                                     <h5>Register</h5>
                                 </Link>
-                                <Link to="/login" className="btn btn-3 col-1">
-                                    <h5>Login</h5>
-                                </Link>
+
+                                {user === "Bạn" ?
+                                    <Link to="/login" className="btn btn-3 col-1">
+                                        <h5>Login</h5>
+                                    </Link> :
+                                    <button onClick={() => { setUser("Bạn") }} className="btn btn-3 col-1">Log Out</button>}
                                 <button type="button" className="btn btn-secondary col-1" onClick={toggle}>
                                     <img src={cart} alt="cart icon" width="25px" height="25px" />
                                 </button>
@@ -43,7 +50,7 @@ function MainPage() {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
---
+++ restaurant-pos/src/components/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyClvGKVsFKylzRH4Agj1S9h5mjAJ2eYDnQ",
+    authDomain: "tododaily-16c8a.firebaseapp.com",
+    projectId: "tododaily-16c8a",
+    databaseURL: "https://tododaily-16c8a-default-rtdb.firebaseio.com",
+    storageBucket: "tododaily-16c8a.appspot.com",
+    messagingSenderId: "235162023344",
+    appId: "1:235162023344:web:d811f937c80b0bf5215275",
+    measurementId: "G-ZZ02EECN39"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
