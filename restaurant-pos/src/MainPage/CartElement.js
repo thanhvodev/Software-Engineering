98e98cc7fa391a57ebfac49cf73b8b030a4657de
thanhvodev <thanhvo.dev@gmail.com>
2021-11-15 12:37:49 +0700

    Revert "Merge branch 'main' into Thanh"
    This reverts commit 69b5b25e209879501ddbc7a6c6ab2d96100c3e28, reversing
    changes made to 4a5dd83b049cebdca7962e8832002d5697eea98a.

--- restaurant-pos/src/MainPage/CartElement.js
+++ restaurant-pos/src/MainPage/CartElement.js
@@ -1,29 +1,24 @@
-import React from "react";
-import cart from "../img/cart.svg";
-import CartInfo from "./CartInfo";
+import React from 'react';
+import cart from '../img/cart.svg'
+import CartInfo from './CartInfo';
 
 const CartElement = ({ hide }) => {
-    return (
-        <div className="cart-modal">
-            <div className="cart-modal-overlay"></div>
-            <div className="container cart overflow-auto">
-                <div className="sticky-top">
-                    <div className="row">
-                        <div className="col-4 mt-2">
-                            <img src={cart} alt="cart icon" width="25px" height="25px" />
-                            <span>Your Cart</span>
-                        </div>
-                        <div className="col-4"></div>
-                        <button className="col-4 btn btn-secondary" onClick={hide}>
-                            Close
-                        </button>
-                    </div>
-                    <p>_____________________________________________________</p>
-                </div>
-                <CartInfo />
-            </div>
+  return (
+    <div className="container cart overflow-auto">
+      <div className="sticky-top">
+      <div className="row">
+        <div className="col-4 mt-2">
+          <img src={cart} alt='cart icon' width='25px' height='25px' />
+          <span>Your Cart</span>
         </div>
-    );
-};
+          <div className="col-4"></div>
+          <button className="col-4 btn btn-secondary" onClick={hide}>Close</button>
+        </div>
+        <p>_____________________________________________________</p>
+      </div>
+      <CartInfo />
+    </div>
+  );
+}
 
 export default CartElement;
